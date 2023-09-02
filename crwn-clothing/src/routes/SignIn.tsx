1c1c73bd623f6ff600f64d87b5367b255058154e
Caio Ceretta <caioceretta@gmail.com>
2023-09-01 23:17:08 -0300

    Crwn Clothing

--- crwn-clothing/src/routes/SignIn.tsx
+++ crwn-clothing/src/routes/SignIn.tsx
@@ -1,37 +1,7 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
-import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../utils/firebase/firebase.utils'
-
-import SignUp from '../components/SignUp';
-
-const SignIn = () => {
-  // useEffect(() => {
-  //   const getRedirectResultFn = async () => {
-  //     const response = await getRedirectResult(auth);
-      
-  //     if(response) {
-  //       const userDocRef = await createUserDocumentFromAuth(response.user)
-  //     }
-  //   }
-
-  //   getRedirectResultFn()
-  // }, [])
-
-  const logGoogleUser = async () => {
-    const {user} = await signInWithGooglePopup();
-    const userDocRef = await createUserDocumentFromAuth(user)
-    
-
-  }
-
-
+function SignIn() {
   return (
     <div>
-      <h1>SignIn Page</h1>
-      <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
-      <SignUp />
+      <h1>Sign In Page</h1>
     </div>
   )
 }
