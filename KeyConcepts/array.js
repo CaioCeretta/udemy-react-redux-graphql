07d06a60ff727464f0aed4ba6700e4a2788437f8
Caio Ceretta <caioceretta@gmail.com>
2023-09-03 11:01:47 -0300

    Introduced state in classes, and small refresher

---
+++ KeyConcepts/array.js
@@ -0,0 +1,17 @@
+// map
+const myArray = [1, 2, 3, 4];
+
+const map = myArray.map(el => {
+  return el + 1
+})
+
+console.log(map);
+
+
+//filter
+
+//reduce
+
+//find
+
+//includes
---
+++ monster-rolodex-js/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [
+
+      ],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(() => {
+        return { monsters: users}
+      },
+      () => {
+        console.log(this.state)
+      }
+      ))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
